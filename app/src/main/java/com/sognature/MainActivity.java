cdd1d2f5f240d956ab79b3946d0f3698c37d21a4
Siddhartha Maji <siddhartha.ee.13@gmail.com>
2015-07-07 15:54:27 +0530

    ffggf

--- app/src/main/java/com/sognature/MainActivity.java
+++ app/src/main/java/com/sognature/MainActivity.java
@@ -2,9 +2,11 @@ package com.sognature;
 
 import android.gesture.GestureOverlayView;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,16 +19,48 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    GestureOverlayView gestureView;
+    Integer GColor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialize();
+        gestureView.setGestureColor(Color.BLUE);
+
+    }
+
+
+public void clk(View view)
+{
+    String Name = (view).getTag().toString();
+    Log.e("name --> ",Name);
+    if(Name.equals("1"))
+    {
+        GColor = Color.BLACK;
+    }
+    else if(Name.equals("2"))
+    {
+        GColor = Color.BLUE;
+    }
+    else if(Name.equals("3"))
+    {
+        GColor = Color.RED;
+    }
+
+    Toast.makeText(getBaseContext(),"color clicked",Toast.LENGTH_SHORT).show();
+    gestureView.setGestureColor(GColor);
+}
+    private void initialize() {
+        gestureView = (GestureOverlayView) findViewById(R.id.signaturePad);
     }
 
     public void saveSig(View view)
     {
         try {
-            GestureOverlayView gestureView = (GestureOverlayView) findViewById(R.id.signaturePad);
+
             gestureView.setDrawingCacheEnabled(true);
             Bitmap bm = Bitmap.createBitmap(gestureView.getDrawingCache());
             File f = new File(Environment.getExternalStorageDirectory()
